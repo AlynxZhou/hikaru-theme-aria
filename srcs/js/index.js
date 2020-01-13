895e4efbe1f010fd36a1d8dbd1faf6ff1857348f
Alynx Zhou <alynx.zhou@gmail.com>
2020-01-13 17:11:03 +0800

    Dropped jQuery in index.js.

--- srcs/js/index.js
+++ srcs/js/index.js
@@ -2,88 +2,169 @@
 // If some init functions are used for library which is always enabled,
 // and does not depend on templating,
 // init function should be written here.
-(function ($) {
-  $(document).ready(function () {
-    // To top button.
-    $("#back-to-top").on("click", function () {
-      $("body, html").animate({"scrollTop": 0}, 600);
-    });
+function documentReady(callback) {
+  document.readyState !== 'loading'
+    ? callback()
+    : document.addEventListener('DOMContentLoaded', callback);
+}
 
-    $("#reward-button").on("click", function () {
-      if ($("#qr").attr("aria-hidden") === "true") {
-        $("#qr").attr("aria-hidden", "false");
-      } else {
-        $("#qr").attr("aria-hidden", "true");
+function slideUp(target, duration) {
+  duration = duration || 400;
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + "ms";
+  target.style.boxSizing = "border-box";
+  target.style.height = target.offsetHeight + "px";
+  target.offsetHeight;
+  target.style.overflow = "hidden";
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout(function () {
+    target.style.display = "none";
+    target.style.removeProperty("height");
+    target.style.removeProperty("padding-top");
+    target.style.removeProperty("padding-bottom");
+    target.style.removeProperty("margin-top");
+    target.style.removeProperty("margin-bottom");
+    target.style.removeProperty("overflow");
+    target.style.removeProperty("transition-duration");
+    target.style.removeProperty("transition-property");
+  }, duration);
+}
+
+function slideDown(target, duration) {
+  duration = duration || 400;
+  target.style.removeProperty("display");
+  var display = window.getComputedStyle(target).display;
+  if (display === "none") {
+    display = "block";
+  }
+  target.style.display = display;
+  var height = target.offsetHeight;
+  target.style.overflow = "hidden";
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.boxSizing = "border-box";
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + "ms";
+  target.style.height = height + "px";
+  target.style.removeProperty("padding-top");
+  target.style.removeProperty("padding-bottom");
+  target.style.removeProperty("margin-top");
+  target.style.removeProperty("margin-bottom");
+  window.setTimeout(function () {
+    target.style.removeProperty("height");
+    target.style.removeProperty("overflow");
+    target.style.removeProperty("transition-duration");
+    target.style.removeProperty("transition-property");
+  }, duration);
+}
+
+function slideToggle(target, duration) {
+  duration = duration || 400;
+  return window.getComputedStyle(target).display === "none"
+    ? slideDown(target, duration)
+    : slideUp(target, duration);
+}
+
+documentReady(function () {
+  var rewardButton = document.getElementById("reward-button");
+  if (rewardButton != null) {
+    rewardButton.onclick = function () {
+      document.getElementById("qr").getAttribute("aria-hidden") === "true"
+        ? document.getElementById("qr").setAttribute("aria-hidden", "false")
+        : document.getElementById("qr").setAttribute("aria-hidden", "true");
+      slideToggle(document.getElementById("qr"));
+    };
+  }
+
+  document.getElementById("nav-toggle").onclick = function () {
+    document.getElementById("menu").getAttribute("aria-hidden") === "true"
+      ? document.getElementById("menu").setAttribute("aria-hidden", "false")
+      : document.getElementById("menu").setAttribute("aria-hidden", "true");
+    slideToggle(document.getElementById("menu"));
+  };
+
+  var current = new Date().getFullYear().toString();
+  var since = document.getElementById("years-text").innerHTML;
+  if (since.length === 0) {
+    document.getElementById("years-text").innerHTML = current;
+  } else if (since !== current) {
+    document.getElementById("years-text").innerHTML = [
+      since, " - ", current
+    ].join("");
+  }
+
+  // (40em - 0.6em) * 16px
+  // 40 is total size and 0.4 is scroll bar size.
+  // jQuery won"t calculate scroll bar size. But CSS will.
+  var minWidth = Math.round((40 - 0.4) * 16);
+  // Auto hide main nav menus in small screen.
+  if (window.innerWidth <= minWidth) {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("menu").setAttribute("aria-hidden", "true");
+    document.getElementById("nav-toggle").setAttribute(
+      "aria-hidden", "false"
+    );
+  }
+  var windowWidth = window.innerWidth;
+  // Show menu again when window becomes bigger.
+  window.onresize = function () {
+    if (window.innerWidth > minWidth) {
+      document.getElementById("menu").style.display = "";
+      document.getElementById("menu").setAttribute("aria-hidden", "false");
+      document.getElementById("nav-toggle").setAttribute(
+        "aria-hidden", "true"
+      );
+    } else {
+      // Android chrome fires resize when scroll down.
+      // Because it hides address bar to enlarge window height.
+      // To avoid it, check width.
+      if (window.innerWidth !== windowWidth) {
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("menu").setAttribute("aria-hidden", "true");
+        document.getElementById("nav-toggle").setAttribute(
+          "aria-hidden", "false"
+        );
+        windowWidth = window.innerWidth;
       }
-      $("#qr").slideToggle();
-    });
-
-    $("#nav-toggle").on("click", function () {
-      if ($("#menu").attr("aria-hidden") === "true") {
-        $("#menu").attr("aria-hidden", "false");
-      } else {
-        $("#menu").attr("aria-hidden", "true");
-      }
-      $("#menu").slideToggle();
-    });
-
-    var current = new Date().getFullYear().toString();
-    var since = $("#years-text").text()
-    if (since.length === 0) {
-      $("#years-text").text(current);
-    } else if (since !== current) {
-      $("#years-text").text([since, " - ", current].join(""));
     }
+  }
 
-    // (40em - 0.6em) * 16px
-    // 40 is total size and 0.4 is scroll bar size.
-    // jQuery won't calculate scroll bar size. But CSS will.
-    var minWidth = Math.round((40 - 0.4) * 16);
-    // Auto hide main nav menus in small screen.
-    if ($(window).width() <= minWidth) {
-      $("#menu").hide();
-      $("#menu").attr("aria-hidden", "true");
-      $("#nav-toggle").attr("aria-hidden", "false");
-    }
-    var windowWidth = $(window).width();
-    // Show menu again when window becomes bigger.
-    $(window).resize(function () {
-      if ($(window).width() > minWidth) {
-        $("#menu").show();
-        $("#menu").attr("aria-hidden", "false");
-        $("#nav-toggle").attr("aria-hidden", "true");
-      } else {
-        // Android chrome fires resize when scroll down.
-        // Because it hides address bar to enlarge window height.
-        // To avoid it, check width.
-        if ($(window).width() !== windowWidth) {
-          $("#menu").hide();
-          $("#menu").attr("aria-hidden", "true");
-          $("#nav-toggle").attr("aria-hidden", "false");
-          windowWidth = $(window).width();
-        }
-      }
-    });
-
-    $(".post img").each(function (i, e) {
-      // If an image works as link, don't attach light gallary to it.
-      if ($(e).parent().prop("tagName") !== "A") {
+  Array.prototype.forEach.call(
+    document.querySelectorAll(".post img"),
+    function (e, i) {
+      // If an image works as link, don"t attach light gallary to it.
+      if (e.parentNode.tagName !== "A") {
         if (e.title) {
-          $(e).after(
+          e.insertAdjacentElement(
+            'afterend',
             ["<span class=\"caption\">", e.title, "</span>"].join("")
           );
         }
-        $(e).wrap(
-          ["<a href=\"", e.src, "\" class=\"gallery-item\"></a>"].join("")
-        );
+        var a = document.createElement('a');
+        a.href = e.src;
+        a.className = "gallery-item";
+        e.insertAdjacentElement('afterend', a);
+        a.appendChild(e);
       } else {
-        $(e).parent().addClass("img-link");
+        e.parentNode.classList.add("img-link");
       }
-    });
-    if (typeof lightGallery != "undefined") {
-      $(".post").each(function (i, e) {
-        lightGallery(e, {"selector": ".gallery-item"});
-      });
     }
-  });
-})(jQuery);
+  );
+
+  if (typeof lightGallery !== "undefined") {
+    Array.prototype.forEach.call(
+      document.querySelectorAll(".post"),
+      function (e, i) {
+        lightGallery(e, {"selector": ".gallery-item"});
+      }
+    );
+  }
+});
